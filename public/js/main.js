42377465c9129920ff04bb45a43d8ab4d0866d21
georges <gf.nodari@hotmail.com>
2021-10-19 14:32:33 +0200

    project init

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+// const reverseNumber = (num) => {
+//     return parseFloat(num.toString().split('').reverse().join(''))
+// }
+// console.log(reverseNumber(876));
+
+// exo2
+const divPar2 = (nbr){
+    switch (nbr/2) {
+        case true:
+            `le num ${nbr} est divisible par 2`
+            break;
+    
+        default:
+            "ce nombre nest pas divisible par 2"
+            break;
+    }
+}
+
+// exo3
+
+// exo4
